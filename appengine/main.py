f1bfd4d6748bee8eb30d50fc449b1436f4ba27e0
Will Moss <will.moss@airbnb.com>
2020-04-21 16:32:16 -0700

    Add code to actually hit the local beagleboard

--- appengine/main.py
+++ appengine/main.py
@@ -1,12 +1,20 @@
 import webapp2
 import json
 import logging
+import socket
+from google.appengine.api import urlfetch
 
 
 class RequestHandler(webapp2.RequestHandler):
     def post(self):
         logging.info('Body: ' + repr(self.request.body))
         logging.info('Headers: ' + repr(self.request.headers))
+
+        result = urlfetch.fetch('http://52.119.125.163:5000/lights')
+        if result.status_code == 201:
+            logging.info('Successfully hit beagleboard')
+        else:
+            logging.error('failed to hit beagleboard, status code ' + result.status_code)
 
         response = {
             'speech': 'Done.',
